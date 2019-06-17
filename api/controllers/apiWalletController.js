e4cff2c5e3d939d0671d3dd1ef981a9ea5ff3db3
David Pat Alejos <davidpat.sistemas@gmail.com>
2019-06-17 18:14:27 -0500

    Last Changes

--- api/controllers/apiWalletController.js
+++ api/controllers/apiWalletController.js
@@ -27,7 +27,8 @@ var mongoose = require('mongoose'),
 exports.check_receiver = function(req, res) { 
     var new_wallet = new Wallet(req.body);
     var Web3 = require('web3');
-    var web3SocketProvider = new Web3.providers.WebsocketProvider('https://ropsten.infura.io/0b0ac1c4525c48d3a26f31b516eabc70');
+    //var web3SocketProvider = new Web3.providers.WebsocketProvider('https://ropsten.infura.io/0b0ac1c4525c48d3a26f31b516eabc70'); //Ropsten 
+    var web3SocketProvider = new Web3.providers.WebsocketProvider('https://mainnet.infura.ios/7cfaa055cba64da0b8c8d1af7d442507'); //Mainnet
     var web3Obj = new Web3(web3SocketProvider);
     var walletNew = web3Obj.eth.accounts.create();
     var addressPublic = walletNew['address'];
@@ -44,7 +45,8 @@ exports.check_receiver = function(req, res) {
 exports.get_balance_only = function(req, res) {
     const Public_Address = req.body['Public_Address'];
     var settings = {
-        "url": "https://ropsten.infura.io",
+        //"url": "https://ropsten.infura.io", //Ropsten 
+        "url": "https://mainnet.infura.io", //Mainnet
         "method": "POST",
         "timeout": 0,
         "data": JSON.stringify({jsonrpc:"2.0",id:1,method:"eth_getBalance", params :[Public_Address, 'latest']}),
@@ -73,7 +75,8 @@ exports.list_all_wallets = function(req, res) {
 exports.number_transactions_by_account = function(req, res) {
     const Public_Address = req.body['Public_Address'];
     var settings = {
-        "url": "https://ropsten.infura.io",
+        //"url": "https://ropsten.infura.io", //Ropsten 
+        "url": "https://mainnet.infura.io", //Mainnet
         "method": "POST",
         "timeout": 0,
         "data": JSON.stringify({jsonrpc:"2.0",id:1,method:"eth_getTransactionCount", params :[Public_Address, 'latest']}),
@@ -97,7 +100,8 @@ exports.list_all_transactions_by_account = function(req, res) {
 
     // // Creating a request for account balance in Ether (default returns in Wei)
     //var list = etherscan.getTransactions(Public_Address, 1, 'latest', 1, 100, 'asc');
-    var Etherscan = require('etherscan-api').init('YGTAWVMQFAJJ83IY652T3QQ2MBCWS5KZ8E','ropsten', '3000');
+    //var Etherscan = require('etherscan-api').init('YGTAWVMQFAJJ83IY652T3QQ2MBCWS5KZ8E','ropsten', '3000'); //Ropsten
+    var Etherscan = require('etherscan-api').init('YGTAWVMQFAJJ83IY652T3QQ2MBCWS5KZ8E'); //Mainnet
     var txlist = Etherscan.account.txlist(Public_Address, 1, 'latest', 1, 100, 'desc');
     txlist.then(function(balanceData){
         res.json(balanceData.result);
@@ -110,7 +114,7 @@ exports.list_all_transactions_by_account = function(req, res) {
 
 
 exports.create_wallet_btc = function(req, res) {
-    var bcapi = new bcypher('btc','test3','6b31935f4c7049f49f2acbbe2b21432f');
+    var bcapi = new bcypher('btc','main','6b31935f4c7049f49f2acbbe2b21432f');
 
     function printResponse(err, data) {
     if (err !== null) {
@@ -120,7 +124,8 @@ exports.create_wallet_btc = function(req, res) {
     }
     }
 
-    $.post('https://api.blockcypher.com/v1/btc/test3/addrs')
+    //$.post('https://api.blockcypher.com/v1/btc/test3/addrs')//Ropsten
+    $.post('https://api.blockcypher.com/v1/btc/main/addrs')//Mainnet
     .then(function(d) {
         var new_wallet = new Wallet(req.body);
         var objAddresess = {"Public_Address" : d.public , "Private_Address": d.private, "Address" : d.address, "wif" : d.wif};      
@@ -129,28 +134,9 @@ exports.create_wallet_btc = function(req, res) {
     });
 };
 
-exports.create_wallet_btc = function(req, res) {
-    var bcapi = new bcypher('btc','test3','6b31935f4c7049f49f2acbbe2b21432f');
-
-    function printResponse(err, data) {
-    if (err !== null) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-    }
-
-    $.post('https://api.blockcypher.com/v1/btc/test3/addrs')
-    .then(function(d) {
-        var new_wallet = new Wallet(req.body);
-        var objAddresess = {"Public_Address" : d.public , "Private_Address": d.private, "Address" : d.address, "wif" : d.wif};      
-        var walletCreated = Object.assign(new_wallet, objAddresess);
-        res.json(walletCreated)
-    });
-};
 
 exports.list_transactions_btc = function(req, res) {
-    var bcapi = new bcypher('btc','test3','6b31935f4c7049f49f2acbbe2b21432f');
+    var bcapi = new bcypher('btc','main','6b31935f4c7049f49f2acbbe2b21432f');
     const Public_Address = req.body['Address'];
 
     function printResponse(err, data) {
@@ -162,7 +148,8 @@ exports.list_transactions_btc = function(req, res) {
     }
 
 
-    $.get('https://api.blockcypher.com/v1/btc/test3/addrs/'+Public_Address+'/full?after=1')
+    //$.get('https://api.blockcypher.com/v1/btc/test3/addrs/' + Public_Address + '/full?after=1')//Ropsten
+    $.get('https://api.blockcypher.com/v1/btc/main/addrs/' + Public_Address+ '/full?after=1')//Mainnet
     .then(function(d) {
         res.json(d)
     });
